bb5731bbb0d28a1a0a4f37e72f13d23d7dc4b982
YWX <wxFortranIndent@163.com>
2013-04-20 23:23:12 +0800

    Build VFCTools with WTL71 and VC2002 /MT & Code::Blocks 12.11

--- ClassWizard/VFCTools/StdAfx.h
+++ ClassWizard/VFCTools/StdAfx.h
@@ -12,11 +12,13 @@
 
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 
+#define _WIN32_IE 0x0400
+
 #include <atlbase.h>
 #include <atlapp.h>
 //You may derive a class from CComModule and use it if you want to override
 //something, but do not change the name of _Module
-extern CComModule _Module;
+//extern CComModule _Module;
 #include <atlcom.h>
 #include <atlmisc.h>
 
